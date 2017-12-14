e980e341e5be1a7ce352bcb1acd07c9e2af03343
Aegis18 <rangeroverkill@gmail.com>
2017-12-14 13:40:01 -0500

    MatchMakingEngine & Interface

---
+++ ServerProperties/src/MatchMakingEngine.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class MatchMakingEngine implements MatchMakingInterface{
+	
+	//TODO
+	//Set up a score for each user
+	//Set up a priority based on location
+	
+	@Override
+	public UserAccountInterface matchMake(List<UserAccountInterface> ListOfUser) {
+		// TODO Auto-generated method stub
+		List<Integer> score = new ArrayList<Integer>();
+		
+		return null;
+	}
+	
+}
---
+++ ServerProperties/src/MatchMakingInterface.java
@@ -0,0 +1,5 @@
+import java.util.*;
+
+public interface MatchMakingInterface {
+	abstract public UserAccountInterface matchMake(List<UserAccountInterface> ListOfUser);
+}
